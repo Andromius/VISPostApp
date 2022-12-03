fc4d3e6fdc04f4593b80f26543ad0510a5ad9be0
Andromius <105814709+Andromius@users.noreply.github.com>
2022-12-04 00:12:46 +0100

    added new project, changed DO to lib

---
+++ PresentationLayer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.DataAccess;
+using DomainObjects.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        City c;
+        CityDataMapper cdm;
+    }
+}
